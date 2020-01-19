6a88aea1d39b524a59a747dfcd7a29229a8aec31
Tim <readatontim@gmail.com>
2020-01-18 23:53:40 -0800

    ignoreme test

---
+++ Extension/query.py
@@ -0,0 +1,34 @@
+import os
+from ignorethis import code
+from google.cloud import language_v1
+from google.cloud.language_v1 import enums
+from googlesearch import search
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = code
+
+def get_data_info(text):
+    client = language_v1.LanguageServiceClient()
+
+    # text_content = 'California is a state.'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = enums.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "en"
+    document = {"content": text, "type": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = enums.EncodingType.UTF8
+
+    response = client.analyze_entities(document, encoding_type=encoding_type)
+    # print([b.name for b in response.entities])
+    return [b.name for b in response.entities if b.salience > 0.01]
+
+important_list = get_data_info("A family crisis: What's in store for the British royals")
+query = " ".join(important_list)
+
+for j in search(query, tld = "co.in", num = 10, stop = 5, pause = 2):
+    print(j)
